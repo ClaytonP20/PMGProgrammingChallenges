6660c1f0979f91cedfd5cfd586af8acb15b5cc81
ClaytonP20 <146488983+ClaytonP20@users.noreply.github.com>
2023-09-30 23:33:40 -0400

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -1,21 +1,26 @@
 -- Clayton Perona // PMG Technical Assessment // September 29, 2023
+
 -- Write a query to get the sum of impressions by day.
-SELECT SUM(impressions), date
+SELECT SUM(impressions), DATE(date) AS day
 FROM marketing_data
 GROUP BY date;
+
 
 -- Write a query to get the top three revenue-generating states in order of best to worst. How much revenue did the third best state generate?
 SELECT SUM(revenue) AS SumRevenue, state
 FROM website_revenue
 GROUP BY state
 ORDER BY SumRevenue DESC
-LIMIT 3;
+LIMIT 3; 
+-- Ohio was the third best state and generated $37,577 in revenue.
+
 
 -- Write a query that shows total cost, impressions, clicks, and revenue of each campaign. Make sure to include the campaign name in the output.
 SELECT campaign_info.name, marketing_data.cost, marketing_data.impressions, marketing_data.clicks, website_revenue.revenue
 FROM campaign_info
 JOIN marketing_data ON campaign_info.id=marketing_data.campaign_id
-JOIN website_revenue ON campaign_info.id=website_revenue.campaign_id;
+JOIN website_revenue ON campaign_info.id=website_revenue.campaign_id
+GROUP BY name;
 
 -- Write a query to get the number of conversions of Campaign5 by state. Which state generated the most conversions for this campaign?
 
