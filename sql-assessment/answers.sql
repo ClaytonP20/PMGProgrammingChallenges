5de15f7bb7f10cd3c04cc8fd493daba3b94184fe
ClaytonP20 <146488983+ClaytonP20@users.noreply.github.com>
2023-10-01 00:14:17 -0400

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -7,16 +7,21 @@ GROUP BY date;
 
 
 -- Write a query to get the top three revenue-generating states in order of best to worst. How much revenue did the third best state generate?
-SELECT SUM(revenue) AS SumRevenue, state
+SELECT state, SUM(revenue) AS Revenue
 FROM website_revenue
 GROUP BY state
-ORDER BY SumRevenue DESC
+ORDER BY Revenue DESC
 LIMIT 3; 
 -- Ohio was the third best state and generated $37,577 in revenue.
 
 
 -- Write a query that shows total cost, impressions, clicks, and revenue of each campaign. Make sure to include the campaign name in the output.
-SELECT campaign_info.name, marketing_data.cost, marketing_data.impressions, marketing_data.clicks, website_revenue.revenue
+SELECT 
+  campaign_info.name AS "Campaign Name", 
+  ROUND(SUM(marketing_data.cost), 2) AS Cost, 
+  SUM(marketing_data.impressions) AS Impressions, 
+  SUM(marketing_data.clicks) AS Clicks, 
+  SUM(website_revenue.revenue) AS Revenue
 FROM campaign_info
 JOIN marketing_data ON campaign_info.id=marketing_data.campaign_id
 JOIN website_revenue ON campaign_info.id=website_revenue.campaign_id
